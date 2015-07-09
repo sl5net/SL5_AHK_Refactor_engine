02c014ccdb96a687a7ae92fed603810ebc22ed4f
Sebastian <SL5@SL5.NET>
2015-07-09 18:32:37 +0200

    no message

--- phpdesktop-msie-1.14-php-5.4.33/www/SL5_preg_contentFinder/examples/AutoHotKey/create_input_file.php
+++
@@ -1,7 +0,0 @@
-<?php
-//include_once 'SUtility.ahk'
-$file_content_original = file_get_contents('SUtility.ahk');
-$file_content_compressed_1 = preg_replace("/\s+/", " ", $file_content_original);
-$file_content_compressed_2 = preg_replace("/[\r\n]\s+/", "\r\n", $file_content_original);
-file_put_contents('input_compressed_1.ahk',$file_content_compressed_1);
-file_put_contents('input_compressed_2.ahk',$file_content_compressed_2);
--- phpdesktop-msie-1.14-php-5.4.33/www/SL5_preg_contentFinder/lib/simpletest_1.1.0/test/support/test1.php
+++
@@ -1,8 +0,0 @@
-<?php
-include_once '../../unit_tester.php';
-class test1 extends UnitTestCase {
-	function test_pass(){
-		$this->assertEqual(3,1+2, "pass1");
-	}
-}
-?>
